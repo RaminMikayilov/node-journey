e8ee56fad556a55bf56b9922515859026d1de494
RaminMikayilov <raminmikayilov2003@gmail.com>
2025-01-04 04:07:49 +0400

    Add global.js with examples of global object, __dirname, and __filename

---
+++ basics/global.js
@@ -0,0 +1,8 @@
+// global object is a global object that provides variables and functions that are available anywhere.
+console.log(global);
+
+// __dirname - path to current directory
+console.log(__dirname);
+
+// __filename - path to the current file
+console.log(__filename);
