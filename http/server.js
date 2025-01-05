fdc0109791e2860f148e16f036851589a5f10a59
RaminMikayilov <raminmikayilov2003@gmail.com>
2025-01-06 01:09:44 +0400

    Add dynamic routing and 404 error page to HTTP server

--- http/server.js
+++ http/server.js
@@ -3,13 +3,27 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
-  fs.readFile("./views/index.html", (err, data) => {
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
     } else {
-      res.write(data);
-      res.end();
+      // res.write(data); // if we have multiple write statements, we can use this
+      res.end(data); // if we have only one write statement, we can use this
     }
   });
 });
