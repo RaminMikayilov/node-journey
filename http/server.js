8be7453d23f50f3678a166151add9ac7518b8ee4
RaminMikayilov <raminmikayilov2003@gmail.com>
2025-01-06 01:15:32 +0400

    Set HTTP status codes for routes in the server

--- http/server.js
+++ http/server.js
@@ -3,17 +3,20 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
-  let path = "./views/";
 
+  let path = "./views/";
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
+      res.statusCode = 200;
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
 
