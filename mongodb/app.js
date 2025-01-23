c29a5e52f71fa57c1f424afd6fc2f78dc217944a
RaminMikayilov <raminmikayilov2003@gmail.com>
2025-01-24 00:22:27 +0400

    Add MongoDB integration with Mongoose and create blog model

---
+++ mongodb/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dbURI = "";
+const Blog = require("./models/blog");
+
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+app.get("/create-blog", (req, res) => {
+  const blog = new Blog({
+    title: "My Second Blog",
+    content: "Hello, this is my second blog post.",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
